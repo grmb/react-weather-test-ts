e2ab10404fd5f8d84a75ff49308a3bd97a8431c6
Gabriel Burgos <gabbyburgos@hotmail.com>
2025-08-04 14:23:07 +0800

    Added useWeatherData hook

---
+++ src/hooks/useWeatherData.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { fetchWeatherData, fetchForecastData } from "../services/weatherApi";
+import { normalizeForecastData, normalizeWeatherData } from "../utils/normalizeWeatherData";
+import { isEmpty } from "lodash";
+
+export const useWeatherData = (defaultCity = "Singapore") => {
+    const [weatherData, setWeatherData] = useState({});
+    const [forecast, setForecast] = useState([]);
+    const [city, setCity] = useState(defaultCity);
+    const [searchInput, setSearchInput] = useState("");
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    
+    const loadWeatherData = async (cityName: string) => {
+        try {
+            const rawWeatherData = await fetchWeatherData(cityName);
+            const rawForecastData = await fetchForecastData(cityName);
+            setCity(cityName);
+            if (!isEmpty(rawWeatherData) && !isEmpty(rawForecastData)) {
+                setWeatherData(normalizeWeatherData(rawWeatherData));
+                setForecast(normalizeForecastData(rawForecastData));
+            } else {
+                throw new Error("Either weather or forecast data are empty.");
+            }
+        } catch (err) {
+            console.log(err);
+            setError("Could not fetch weather data, please try again.");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    useEffect(() => {
+        loadWeatherData(city);
+    }, [city])
+
+    return {
+        city,
+        searchInput,
+        setSearchInput,
+        weatherData,
+        forecast,
+        error,
+        isLoading,
+        fetchWeatherData: loadWeatherData
+    }
+}
